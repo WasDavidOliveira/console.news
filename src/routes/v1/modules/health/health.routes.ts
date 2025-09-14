75f60f83ef82e86c47f6ab34c146ae0e5ea1d929
David Oliveira <daviddenisson2014@gmail.com>
2025-09-14 19:27:58 -0300

    Remove middleware de autenticação e verificação de papel da rota de health check, permitindo acesso irrestrito ao endpoint de saúde. Ajusta a rota detalhada para incluir novamente os middlewares, garantindo que apenas administradores possam acessá-la.

--- src/routes/v1/modules/health/health.routes.ts
+++ src/routes/v1/modules/health/health.routes.ts
@@ -1,10 +1,12 @@
 import { Router } from 'express';
 import HealthController from '@/controllers/v1/modules/health/health.controller';
+import { authMiddleware } from '@/middlewares/auth/auth.middlewares';
+import { hasRole } from '@/middlewares/authorization/role.middleware';
 
 const router: Router = Router();
 
 router.get('/', HealthController.simple);
 
-router.get('/detailed', HealthController.check);
+router.get('/detailed', authMiddleware, hasRole('admin'), HealthController.check);
 
 export default router;
